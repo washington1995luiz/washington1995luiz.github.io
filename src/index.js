c4ae0d3c584faa87bef61171b00a613a516ac910
washington1995luiz <35905270+washington1995luiz@users.noreply.github.com>
2023-12-03 04:46:08 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // script.js
 const form = document.getElementById("form");
 const server = "https://whatsapp-u6dl.onrender.com";
-const socket = new WebSocket(`wss://whatsapp-u6dl.onrender.com:8081`);
+const socket = new WebSocket(`wss://whatsapp-u6dl.onrender.com`);
 
 let animationInitialized = false;
 let countAnimation = 0;
